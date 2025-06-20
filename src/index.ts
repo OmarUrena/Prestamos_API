487904918802dc424e789d69c1e72bfd5f14ffe6
Omar Ureña <1988informatica@gmail.com>
2025-06-20 00:42:56 -0400

    ..

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,8 @@ dotenv.config();
 
 const app = express();
 const corsOptions = {
-  origin: '*',
+  origin: '"http://localhost:3000"',
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
